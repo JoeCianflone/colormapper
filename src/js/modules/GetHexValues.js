33501998d351080ead1ac05c192e2e9cb1f0237a
Joe Cianflone <joe@cianflone.co>
2016-01-12 22:40:51 -0500

    visual updates

--- src/js/modules/GetHexValues.js
+++ src/js/modules/GetHexValues.js
@@ -3,11 +3,11 @@ App.Modules.GetHexValues = function () {
    var o = { };
 
    var getTheHex = function(data) {
-      var hexVal = $('.js-hex-code').val();
+      var hexVal = $('.js-hex-code').val().replace("#", "");
       var match = straightMatch(hexVal);
 
-      if (!match) {
-         match = calcValue(hexVal);
+      if (! match) {
+         match = findClosestMatch(hexVal, colorList);
       }
 
       $(".js-show").html(match);
@@ -17,14 +17,43 @@ App.Modules.GetHexValues = function () {
       return _.isUndefined(colorList[color]) ? false : colorList[color];
    };
 
-   var calcValue = function (color) {
+   var findClosestMatch = function(hexColor, colors) {
+       var rgb = hex2rgb(hexColor),
+           gap = 3 * Math.pow(256, 2) + 1,
+           closest = null;
 
+       _.each(colors, function(value, key, index) {
+         var diff = rgbDistance(hex2rgb(key), rgb);
+         if (diff < gap) {
+             gap = diff;
+             closest = value;
+         }
+       });
+
+       return closest;
+   };
+
+   var hex2rgb = function(hex) {
+      hex = parseInt(hex, 16);
+      var r = hex >> 16;
+      var g = hex >> 8 & 0xFF;
+      var b = hex & 0xFF;
+
+      return {
+         r: r,
+         g: g,
+         b: b
+      };
+   };
+
+
+   var rgbDistance = function(c1, c2) {
+       return Math.pow(c1.r - c2.r, 2) + Math.pow(c1.g - c2.g, 2) + Math.pow(c1.b - c2.b, 2);
    };
 
    return {
-      init: function() {
-         return this;
-      },
+      init: function() { return this; },
+
       events: function() {
          Events.bind("click", ".js-get-hex").to(getTheHex, this);
          return this;
