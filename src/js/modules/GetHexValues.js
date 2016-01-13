fb9f57f8446066beecd3b6a5727d09f6603c6a1a
Joe Cianflone <joe@cianflone.co>
2016-01-12 23:37:44 -0500

    all the changes from tonight

--- src/js/modules/GetHexValues.js
+++ src/js/modules/GetHexValues.js
@@ -2,28 +2,34 @@ App.Modules = App.Modules || {};
 App.Modules.GetHexValues = function () {
    var o = { };
 
-   var getTheHex = function(data) {
-      var hexVal = $('.js-hex-code').val().replace("#", "");
-      var match = straightMatch(hexVal);
+   var colorLookup = function(data) {
+      var hexList = $('.js-hex-code').val().split(",");
 
-      if (! match) {
-         match = findClosestMatch(hexVal, colorList);
-      }
+      _.each(hexList, function(hexItem) {
+         var hexVal = hexItem.trim().replace("#", ""),
+             match = findMatch(hexVal);
 
-      $(".js-show").html(match);
+         $(".js-show").append(Handlebars.templates.colorSnippet({
+            colorName: match,
+            hexValue: hexVal
+         }));
+         //$(".js-show").append(match + ": #"+hexVal+", <br>");
+
+      });
+
    };
 
-   var straightMatch = function(color) {
-      return _.isUndefined(colorList[color]) ? false : colorList[color];
+   var findMatch = function(color) {
+      return _.isUndefined(colorList[color]) ? findClosestMatch(color, colorList) : colorList[color];
    };
 
    var findClosestMatch = function(hexColor, colors) {
-       var rgb = hex2rgb(hexColor),
+       var rgb = App.Helpers.hex2rgb(hexColor),
            gap = 3 * Math.pow(256, 2) + 1,
            closest = null;
 
        _.each(colors, function(value, key, index) {
-         var diff = rgbDistance(hex2rgb(key), rgb);
+         var diff = App.Helpers.rgbDistance(App.Helpers.hex2rgb(key), rgb);
          if (diff < gap) {
              gap = diff;
              closest = value;
@@ -33,29 +39,11 @@ App.Modules.GetHexValues = function () {
        return closest;
    };
 
-   var hex2rgb = function(hex) {
-      hex = parseInt(hex, 16);
-      var r = hex >> 16;
-      var g = hex >> 8 & 0xFF;
-      var b = hex & 0xFF;
-
-      return {
-         r: r,
-         g: g,
-         b: b
-      };
-   };
-
-
-   var rgbDistance = function(c1, c2) {
-       return Math.pow(c1.r - c2.r, 2) + Math.pow(c1.g - c2.g, 2) + Math.pow(c1.b - c2.b, 2);
-   };
-
    return {
       init: function() { return this; },
 
       events: function() {
-         Events.bind("click", ".js-get-hex").to(getTheHex, this);
+         Events.bind("click", ".js-find-color-name").to(colorLookup, this);
          return this;
       }
    };
